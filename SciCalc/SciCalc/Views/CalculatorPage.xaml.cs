acac023fc07d50b875352bd7a3f8e6f141c1c71c
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2025-01-29 11:38:41 +0200

    View model

---
+++ SciCalc/SciCalc/Views/CalculatorPage.xaml.cs
@@ -0,0 +1,15 @@
+using ViewModels;
+
+namespace Views;
+
+public partial class CalculatorPage : ContentPage
+{
+	public CalculatorPage()
+	{
+		InitializeComponent();
+
+		this.BindingContext = new CalculatorPageViewModel();
+
+
+    }
+}
