3ffc2caedea02838d83dabcf965680ce8674ac30
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2025-02-11 14:11:41 +0200

    PlanetsPage and PlanetsDetailsPage

---
+++ MauiPlanets/MauiPlanets/Views/PlanetsDetailsPage.xaml.cs
@@ -0,0 +1,12 @@
+using Models;
+
+namespace Views;
+
+public partial class PlanetsDetailsPage : ContentPage
+{
+	public PlanetsDetailsPage(Planet planet)
+	{
+		InitializeComponent();
+		this.BindingContext = planet;
+	}
+}
--- MauiPlanets/MauiPlanets/Views/PlanetsPage.xaml.cs
+++ MauiPlanets/MauiPlanets/Views/PlanetsPage.xaml.cs
@@ -26,4 +26,11 @@ public partial class PlanetsPage : ContentPage
 	{
 		await CloseMenu();
 	}
+
+	private async Task CloseMenu()
+	{
+		_ = MainContentGrid.FadeTo(1, AnimationDuration);
+		_ = MainContentGrid.ScaleTo(1, AnimationDuration);
+		await MainContentGrid.TranslateTo(0,0,AnimationDuration,Easing.CubicIn);
+	}
 }
