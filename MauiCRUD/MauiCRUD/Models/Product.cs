567bc0fc6a36853b99bd01c3a0712adbfe5d823b
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2025-02-19 11:31:58 +0200

    Database context and product model

--- MauiCRUD/MauiCRUD/Models/Product.cs
+++ MauiCRUD/MauiCRUD/Models/Product.cs
@@ -14,5 +14,18 @@ namespace Models
         {
             return MemberwiseClone() as Product;
         }
+
+        public (bool IsValid, string? ErrorMessage) Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                return (false, $"{nameof(Name)} is required.");
+            }
+            if (Price <= 0)
+            {
+                return (false, $"{nameof(Price)} has to be greater than 0");
+            }
+            return (true, null);
+        }
     }
 }
