e53f501244518a8a328d70414c28d800ba763d7f
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2025-02-21 14:11:11 +0200

    Viewmodel and mainpage

--- MauiCRUD/MauiCRUD/MainPage.xaml.cs
+++ MauiCRUD/MauiCRUD/MainPage.xaml.cs
@@ -1,24 +1,21 @@
-﻿namespace MauiCRUD
+﻿using ViewModels;
+
+namespace MauiCRUD
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
-        public MainPage()
+        private readonly ProductsViewModel _productsViewModel;
+        public MainPage(ProductsViewModel productsViewModel)
         {
             InitializeComponent();
+            _productsViewModel = productsViewModel;
+            BindingContext = productsViewModel;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        protected async override void OnAppearing()
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            base.OnAppearing();
+            await _productsViewModel.LoadProductsAsync();
         }
     }
 
--- MauiCRUD/MauiCRUD/ViewModels/ProductsViewModel.cs
+++ MauiCRUD/MauiCRUD/ViewModels/ProductsViewModel.cs
@@ -1,7 +1,125 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Data;
+using Models;
+using System.Collections.ObjectModel;
 namespace ViewModels
 {
     public partial class ProductsViewModel : ObservableObject
     {
+        private readonly DatabaseContext _context;
+        public ProductsViewModel(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [ObservableProperty]
+        private ObservableCollection<Product> _products = new();
+
+        [ObservableProperty]
+        private Product _operatingProduct = new();
+
+        [ObservableProperty]
+        private bool _isBusy;
+
+        [ObservableProperty]
+        private string _busyText;
+
+        public async Task LoadProductsAsync()
+        {
+            await ExecuteAsync(async() =>
+            {
+                var products = await _context.GetAllAsync<Product>();
+                if (Products is not null && products.Any())
+                {
+                    Products ??= new ObservableCollection<Product>();
+                    foreach (var product in products)
+                    {
+                        Products.Add(product);
+                    }
+                }
+            },"Fetching products...");
+        }
+        private async Task ExecuteAsync(Func<Task> operation,string? busyText = null)
+        {
+            IsBusy = true;
+            BusyText = busyText ?? "Processing...";
+            try
+            {
+                await operation?.Invoke();
+            }
+            catch (Exception ex) { }
+            finally
+            {
+                IsBusy = false;
+                BusyText = "Processing...";
+            }
+        }
+
+        [RelayCommand]
+        private void SetOperatingProduct(Product? product)
+        {
+            OperatingProduct = product ?? new();
+        }
+
+        [RelayCommand]
+        private async Task SaveProductAsync()
+        {
+            if (OperatingProduct is null)
+            {
+                return;
+            }
+            var (isValid, errorMessage) = OperatingProduct.Validate();
+            if (!isValid)
+            {
+                await Shell.Current.DisplayAlert("Validation Failed",errorMessage,"OK");
+                return;
+            }
+
+            var busyText = OperatingProduct.Id == 0 ? "Creating product..." : "Updating product";
+            await ExecuteAsync(async () =>
+            {
+                if(OperatingProduct.Id == 0)
+                {
+                    await _context.AddItemAsync<Product>(OperatingProduct);
+                    Products.Add(OperatingProduct);
+                }
+                else
+                {
+                    if (await _context.UpdateItemAsync<Product>(OperatingProduct))
+                    {
+                        var productCopy = OperatingProduct.Clone();
+                        var index = Products.IndexOf(OperatingProduct);
+                        Products.RemoveAt(index);
+                        Products.Insert(index, productCopy);
+                        //Products[index] = productCopy;
+                    }
+                    else
+                    {
+                        await Shell.Current.DisplayAlert("Error","Product updating failed","OK");
+                        return;
+                    }
+                }
+                SetOperatingProductCommand.Execute(new());
+            },busyText);
+        }
+
+        [RelayCommand]
+        private async Task DeleteProduct(int id)
+        {
+            await ExecuteAsync(async () =>
+            {
+                if(await _context.DeleteItemByKeyAsync<Product>(id))
+                {
+                    var product = Products.FirstOrDefault(p=>p.Id == id);
+                    Products.Remove(product);
+                }
+                else
+                {
+                    await Shell.Current.DisplayAlert("Error","Failed to removed product","OK");
+                }
+            },"Deleting product...");
+        }
+
     }
 }
